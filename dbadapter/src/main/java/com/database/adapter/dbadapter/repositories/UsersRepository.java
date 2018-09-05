26b30f280f3ed594bfe839388753606e673eabf9
Neeraj Sabapathy <Neeraj@lawn-128-61-49-188.lawn.gatech.edu>
2018-09-05 11:47:02 -0400

    deleted bad folder

--- dbadapter/src/main/java/com/database/adapter/dbadapter/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.database.adapter.dbadapter.repositories;
-
-import com.database.adapter.dbadapter.domain.Users;
-import com.database.adapter.dbadapter.repositories.BaseRepository;
-
-public interface UsersRepository extends BaseRepository<Users, Integer> {
-
-
-}
