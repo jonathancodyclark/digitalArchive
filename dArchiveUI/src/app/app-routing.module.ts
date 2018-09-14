060e48dc4bf24ea91036005e985f349d0891a26f
kroman6 <kroman6@gatech.edu>
2018-09-14 13:20:40 -0400

    added home page

--- dArchiveUI/src/app/app-routing.module.ts
+++ dArchiveUI/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
+import { HomePageComponent } from './homePage/homePage.component';
 
 import { PageNotFoundComponent } from './not-found/not-found.component';
 import { ExhibitsComponent } from './exhibits/exhibits.component';
@@ -8,11 +9,12 @@ import { ArtifactsComponent } from './artifacts/artifacts.component';
 import { ArtifactDetailComponent } from './artifacts/artifact-detail.component';
  
 const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
   { path: 'exhibits', component: ExhibitsComponent },
   { path: 'exhibit-detail', component: ExhibitDetailComponent },
   { path: 'artifacts/:id', component: ArtifactsComponent },
   { path: 'artifact-detail', component: ArtifactDetailComponent },
-  { path: '',   redirectTo: '/exhibits', pathMatch: 'full' },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
  
