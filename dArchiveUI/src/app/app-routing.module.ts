a877aaad258ec916f0cb8c033b0ad640baf28f38
burketaylor <burkemtaylor@gmail.com>
2018-09-27 17:05:16 -0400

    re-added login routing

--- dArchiveUI/src/app/app-routing.module.ts
+++ dArchiveUI/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { NewUserComponent } from './appusers/newUser.component';
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'manageusers', component: AppUsersComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'newuser', component: NewUserComponent },
   { path: 'exhibits', component: ExhibitsComponent },
   { path: 'exhibit-detail', component: ExhibitDetailComponent },
