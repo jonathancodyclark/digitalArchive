67ea2562895ea53193c144de7d6855e6314d169b
burketaylor <burkemtaylor@gmail.com>
2018-11-09 13:17:41 -0500

    new token

--- dArchiveUI/src/app/services/image.service.ts
+++ dArchiveUI/src/app/services/image.service.ts
@@ -12,7 +12,7 @@ export class ImageService {
 
     editedExhibit = undefined;
     backendUrl = 'http://localhost:8080/storage';
-    headers = new HttpHeaders({'Content-Type':'application/json','Authorization': `${this.cookieService.get('token')}`});
+    headers = new HttpHeaders({'Authorization': `${this.cookieService.get('token')}`});
     options = {headers: this.headers};
 
     constructor(
@@ -23,28 +23,25 @@ export class ImageService {
 
     addImage(artifactId: number, selectedFile : File) { 
 
-        const fd = new FormData();
-        fd.append('file', selectedFile, selectedFile.name)
-        console.log(fd);
-        return this.http.post(this.backendUrl + `/${artifactId}/uploadFile`, fd).subscribe(res => {
+        var fd = new FormData();
+        fd.append('File', selectedFile, selectedFile.name)
+        console.log(fd.get('file'));
+        return this.http.post(this.backendUrl + `/${artifactId}/uploadFile`, fd, this.options).subscribe(res => {
             console.log(res);
         })
     }
 
     editImage(artifactId: number, selectedFile : File) {
         const fd = new FormData();
-        fd.append('file', selectedFile, selectedFile.name)
+        fd.append('File', selectedFile, selectedFile.name)
         console.log(fd);
-        return this.http.post(this.backendUrl + `/${artifactId}/editFile`, fd).subscribe(res => {
+        return this.http.post(this.backendUrl + `/${artifactId}/editFile`, fd, this.options).subscribe(res => {
             console.log(res);
         })
     }
 
     deleteImage(artifactId: number, filePath : string) { 
-        var fd = new FormData();
-        fd.append('file', null, '')
-        console.log(fd);
-        return this.http.post(this.backendUrl + `/${artifactId}/editFile`, fd).subscribe(res => {
+        return this.http.delete(this.backendUrl + `/${artifactId}/deleteFile`, this.options).subscribe(res => {
             console.log(res);
         })
     }
